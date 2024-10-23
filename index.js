7ba31b22e973444b28cf9f53673090efb70e6ff1
larryruto98 <larryruto@gmail.com>
2024-10-23 12:59:10 +0300

    completed functions lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+const hq = 42; 
+
+function distanceFromHqInBlocks(customer) {
+    return Math.abs(hq - customer); 
+}
+
+function distanceFromHqInFeet(customer){
+    return (distanceFromHqInBlocks(customer) * 264)
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(((start- destination) * 264))
+  }
+  function calculatesFarePrice(start, destination){
+    const distance = ((Math.abs(destination -start) * 264))
+
+  if (distance <= 400) {
+      return 0;
+  } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400 )* 0.02
+  } else if (distance > 2000 && distance <= 2500) {
+      return 25
+  } else {
+      return "cannot travel that far"; 
+  }
+}
